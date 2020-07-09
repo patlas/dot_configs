164200c3f639190ca0750cba9fcc8096c19a8e65
PatLas <patlas92@wp.pl>
2020-07-09 07:49:08 +0200

    todo - add pylint

--- docker-vim/docker_run.sh
+++ docker-vim/docker_run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run  -it  debian_vim
+docker run  -v /krksrvbuild07_ws/p.las2:/home/krksrvbuild07_ws -it  debian_vim
+#docker run  -v /krksrvbuild07_ws:/krksrvbuild07_ws -w=/krksrvbuild07_ws -it  debian_vim
